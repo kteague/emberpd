9ef5900e7d7947d181a83e022485c81df5ea9c79
Kevin Teague <kevin@bud.ca>
2015-10-05 20:01:51 -0700

    roll signup pages into one template and use controller to change sections displayed

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	firstPage: true,
+	secondPage: false,
+	actions: {
+		secondPage: function() {
+			this.set('firstPage', false);
+			this.set('secondPage', true);
+		}
+	}
 });
--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
+import Profile from '../models/profile';
 
 export default Ember.Route.extend( UnauthenticatedRouteMixin, {
-	actions: {
-		startProfile: function() {
-  		  var profile = this.store.createRecord('profile', {
-  			'username': 'signup',
-          	'orientation': 'straight',
-  			'gender': 'male'
-          });
-		  this.transitionTo('signup2');
-		}
-	}
+
 });
