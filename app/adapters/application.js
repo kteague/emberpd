4ad004219ca3e11e0506277bf92647f5534b3929
Kevin Teague <kevin@bud.ca>
2015-10-06 00:03:40 -0700

    send request to API server, move API server behind a reverse proxy

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
-	host: 'http://0.0.0.0:6543',
+	host: 'http://0.0.0.0:80',
 	namespace: 'api/1',
 	authorizer: 'authorizer:application'
 });
