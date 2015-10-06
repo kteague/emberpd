2584dd8cabffb610b9d040a681b157b6d937cde4
Kevin Teague <kevin@bud.ca>
2015-10-05 20:56:43 -0700

    use Jquery to toggle form visiblity to switch pages

--- app/controllers/signup.js
+++ app/controllers/signup.js
@@ -1,12 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-	firstPage: true,
-	secondPage: false,
 	actions: {
 		secondPage: function() {
-			this.set('firstPage', false);
-			this.set('secondPage', true);
+			$( ".second-page" ).show();
+			$( ".first-page" ).hide();
 		}
 	}
 });
