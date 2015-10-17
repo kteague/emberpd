86c26d8715a0ee2b331153d49329cb8bf961e6bb
Kevin Teague <kevin@bud.ca>
2015-10-16 22:46:06 -0700

    start adding password set form

--- app/routes/activate.js
+++ app/routes/activate.js
@@ -3,8 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	
 	model(params) {
-		
 		return this.store.queryRecord('signup', params);
 	},
 	
+	actions: {
+
+		setPassword: function() {
+			var _this = this;
+			this.controller.model.save().then(function() {
+				_this.transitionTo('app.home');
+			});
+		}
+
+	}
+
 });
