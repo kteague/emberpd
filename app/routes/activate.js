95cd57544fec667748d865bd8c799b8c69609dfe
Kevin Teague <kevin@bud.ca>
2015-10-18 00:30:03 -0700

    remove cruft

--- app/routes/activate.js
+++ app/routes/activate.js
@@ -16,13 +16,7 @@ export default Ember.Route.extend({
 			$.post('/api/1/set_password', { id: id, k: k, p: password }).then(
 				this.transitionTo('app.home')
 			);
-			
-			//var profile = this.store.createRecord('profile');
-			//profile.set('password', this.controller.get('password'));
-			//profile.set('id', this.controller.model.id);
-			//profile.save().then(function() {
-			//	_this.transitionTo('app.home');
-			//});
+
 		}
 
 	}
