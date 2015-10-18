adcc5b69e1d6cccfe2a464e139e64f2861969209
Kevin Teague <kevin@bud.ca>
2015-10-18 00:34:36 -0700

    cleanup cruft

--- app/fixtures.js
+++
@@ -1,8 +0,0 @@
-Profile.FIXTURES = [
-	{
-		fullName: "Bob Dobber",
-	},
-	{
-		fullName: "Jane Zane",
-	},
-];
--- app/routes/signup.js
+++ app/routes/signup.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import UnauthenticatedRouteMixin from 'ember-simple-auth/mixins/unauthenticated-route-mixin';
-import Profile from '../models/profile';
 
 export default Ember.Route.extend( UnauthenticatedRouteMixin, {
 	
