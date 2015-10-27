1b68d1dec4b8599da9dd382ee9efd3bc9b57d8d9
Kevin Teague <kevin@bud.ca>
2015-10-27 00:12:07 -0700

    start password reset page

--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
     this.route('signup');
+    this.route('passwordreset');
     this.route('welcome');
     this.route('login');
     this.route('about');
