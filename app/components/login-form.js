4b15c855d2d4e59d3c3606be9df72ae123a7b2f8
Kevin Teague <kevin@bud.ca>
2015-10-26 20:52:15 -0700

    add a "logging in" for network delays with a font-awesome spinner

--- app/components/login-form.js
+++ app/components/login-form.js
@@ -5,12 +5,18 @@ const { service } = Ember.inject;
 export default Ember.Component.extend({
   session: service('session'),
 
+  isLoggingIn: false,
+  
   actions: {
-    authenticateWithPd() {
+    authenticateWithPD() {
+      this.set('isLoggingIn', true);
+      
       this.get('session').authenticate(
         'authenticator:pd', this.get('identification'), this.get('password')
       ).catch((reason) => {
-        this.set('errorMessage', reason.error);
+        this.set('errorMessage', 'Login Failed');
+      }).finally( () => {
+        this.controller.set('isLoggingIn', false);
       });
     },
 
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -13,6 +13,7 @@ module.exports = function(defaults) {
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', { destDir: 'fonts' });
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', { destDir: 'fonts' });
   app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', { destDir: 'fonts' });
+  app.import("bower_components/font-awesome/css/font-awesome.css");
 
   return app.toTree();
 };
